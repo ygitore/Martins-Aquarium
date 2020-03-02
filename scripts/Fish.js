f30bf15850525cf72081c057f176f78da80984ac
yitbarekgitore <yitbareky@gmail.com>
2020-03-02 11:51:44 -0600

    added dialog box.

--- scripts/Fish.js
+++ scripts/Fish.js
@@ -9,13 +9,11 @@ const Fish = (fish) => {
             </div>
             <div class="fish__name">${fish.name}</div>
             <button id="button--${fish.name}">Details</button>
-
             <dialog class="dialog--fish" id="details--${fish.name}">
-                <div>Species: ${fish.species}</div>
+                <div>Species: ${fish.Species}</div>
                 <div>Location: ${fish.location}</div>
-                <div>Length: ${fish.size}</div>
+                <div>Length: ${fish.length}</div>
                 <div>Food: ${fish.food.join(",")}</div>
-
                 <button class="button--close">Close Dialog</button>
             </dialog>
         </div>
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,4 @@
 import initializeDetailButtonEvents from './dialog.js';
-// import initializeDetailButtonEventsNat from './nat-dialog.js';
-// import initializeDetailButtonEventsBetty from './betty-dialog.js';
-// import initializeDetailButtonEventsDory from './dory-dialog.js';
-// import initializeDetailButtonEventsLusk from './lusk-dialog.js';
-// import initializeDetailButtonEventsPink from './pink-dialog.js';
-// import initializeDetailButtonEventsNemo from './nemo-dialog.js';
 import intializeFishList from './fishList.js ';
 
 // initializeDetailButtonEventsNemo();
@@ -14,4 +8,4 @@ import intializeFishList from './fishList.js ';
 // initializeDetailButtonEventsBetty();
 // initializeDetailButtonEventsNat();
 intializeFishList();
-// initializeDetailButtonEvents();
+initializeDetailButtonEvents();
